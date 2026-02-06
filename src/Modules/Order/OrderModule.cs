2a229b2f50dd4e64e847c47176e2f83786a5c019
Youssef Mohamed <yomohu2000@gmail.com>
2026-02-06 02:27:29 +0200

    feat: add Order module

---
+++ src/Modules/Order/OrderModule.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EShopModularMonolith.Modules.Order;
+
+public static class OrderModule
+{
+    public static IServiceCollection AddOrderModule(this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        return services;
+    }
+}
